c766c968b8983cc865a1dc1527dcc0eef91c2f47
stasPromist <ctacbars@gmail.com>
2022-09-15 21:59:28 +0300

    changes

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Card } from '../models/card.model';
 import { ImagesService } from '../services/images.service';
 @Component({
@@ -11,7 +11,16 @@ import { ImagesService } from '../services/images.service';
 export class ImagesComponent implements OnInit {
 
   constructor(private http: HttpClient, private imgServices: ImagesService) { }
-  API_URL = '';
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  
+   pageNumber = 0;
+   API_URL = `https://picsum.photos/v2/list?page=${this.pageNumber}&limit=3`
+
+  // API_URL = `https://picsum.photos/v2/list?page=${pageNumber}&limit=100`
+  // API_URL = 'https://picsum.photos/v2/list';
+  // API_URL = '';
   // imagesArray = [];
   isButtonDisabled = true;
   imagesArray: Card[] = this.imgServices.imagesArray;
@@ -19,8 +28,9 @@ export class ImagesComponent implements OnInit {
   @ViewChild('url') url!: ElementRef;
   @ViewChild('name') name!: ElementRef;
 
+  // formData = { url: this.API_URL, name: 'xample' }
 
-  formData = { url: this.API_URL, name: 'xample' }
+  formData = { url: "example@.com", name: 'xample' }
 
   ngOnInit(): void {
     console.log("ngOnInit")
@@ -53,19 +63,24 @@ export class ImagesComponent implements OnInit {
  
 
   addRandomImages() {
-    this.http.get(this.formData.url).subscribe({
-
+    this.pageNumber++
+    this.API_URL=`https://picsum.photos/v2/list?page=${this.pageNumber}&limit=3`
+    this.http.get(this.API_URL).subscribe({
       next: (imagesArray: any) => {
         imagesArray.forEach((image: any) =>
           this.imgServices.imagesArray.push(new Card(image.author, image.download_url)))
           console.log(imagesArray);
+         
+          
         // this.formData.name = ''
-        this.formData.url = ''
+        // this.formData.url = ''
       },
+     
       error: (e) => console.error(e),
       complete: () => console.info('complete')
 
     })
+    
   }
 
   post() {
@@ -75,6 +90,7 @@ export class ImagesComponent implements OnInit {
       url: "this.formData.url",
       description: "Test"
     }), { headers: myHeaders }).subscribe(data => console.log(data));
+
   }
 
   ngOnDestroy() {
--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { Card } from '../models/card.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImagesService {
+  
 
   constructor() { }
-
+  
   imagesArray:Card[] = [];
 
 removeImageById(id:string) {
